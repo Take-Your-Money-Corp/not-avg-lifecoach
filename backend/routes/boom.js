b44bac14d04d6178a179f86531f4c6eea5536383
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-02-15 10:09:46 -0800

    frontend and backend working

--- backend/routes/boom.js
+++ backend/routes/boom.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/boom', function (req, res, next) {
-    res.send('respond with a resource');
+/* GET boom */
+router.get('/', function (req, res, next) {
+
+    res.send('BOOM!');
 });
 
 module.exports = router;
