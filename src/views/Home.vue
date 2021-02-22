25494d87a10ef4f6fabd7649e754c292578b2014
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-02-22 14:01:40 -0800

    change file names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ChatBot msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ChatBot from "@/components/ChatBot.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    ChatBot
   }
 };
 </script>
