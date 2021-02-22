b447927c94130119a4534d48d5892a2b2170d5a9
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-02-22 12:44:28 -0800

    send message to chat api and receive reply from bot

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const { dockStart } = require("@nlpjs/basic");
 const { RestConnector } = require("@nlpjs/rest-connector");
 
-(async() => {
+(async () => {
     const dock = await dockStart();
     const container = dock.getContainer();
     container.use(RestConnector)
     await container.get('rest').start();
     const nlp = dock.get('nlp');
-
     await nlp.train();
+    await nlp.addCorpus('./corpus.json');
+
 })();
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="hello">
-    <button @click="submit">GET Data</button>
+    <button @click="submit">Make Conversation</button>
+    <p>The convo Id is: {{ convoId }}</p>
+    <button @click="sendMessage">Send Message</button>
+    <!-- <p>The reply is {{ reply }}</p> -->
+    <button @click="getReply">Get Reply</button>
+    <p>The reply is {{ reply }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
+  data() {
+    return {
+      convoId: "",
+      reply: ""
+    };
+  },
   name: "HelloWorld",
   props: {
     msg: String
@@ -14,12 +25,59 @@ export default {
   methods: {
     submit() {
       axios
-        .get("http://localhost:3000/boom")
-        .then(function(response) {
-          alert(response.data);
+        .get("http://localhost:3000/rest/token")
+        .then(response => {
+          console.log(response.data);
+          this.convoId = response.data.id;
         })
-        .catch(function(error) {
-          alert(error);
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    sendMessage() {
+      axios({
+        method: "post",
+        url: `http://localhost:3000/directline/conversations/${this.convoId}/activities`,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        data: {
+          text: "who are you",
+          textFormat: "plain",
+          type: "message",
+          channelId: "webchat",
+          from: { id: "User", name: "", role: "user" },
+          locale: "es-ES",
+          timestamp: "2021-02-02T14:14:04.268Z",
+          entities: [
+            {
+              requiresBotState: true,
+              supportsListening: true,
+              supportsTts: true,
+              type: "ClientCapabilities"
+            }
+          ]
+        }
+      })
+        .then(response => {
+          console.log(response.data);
+          // this.reply = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getReply() {
+      axios
+        .get(
+          `http://localhost:3000/directline/conversations/${this.convoId}/activities`
+        )
+        .then(response => {
+          this.reply = response.data.activities[1].text;
+          console.log(response.data.activities[1].text);
+        })
+        .catch(error => {
+          console.log(error);
         });
     }
   }
