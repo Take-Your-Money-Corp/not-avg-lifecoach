67537cf6822835f65223ebe34abb80c89ae7af19
Wee Jia Quan <weejiaquan1234@gmail.com>
2021-02-22 11:39:50 -0800

    backend test combine

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const { dockStart } = require("@nlpjs/basic");
+const { RestConnector } = require("@nlpjs/rest-connector");
 
-(async () => {
+(async() => {
     const dock = await dockStart();
+    const container = dock.getContainer();
+    container.use(RestConnector)
+    await container.get('rest').start();
     const nlp = dock.get('nlp');
+
     await nlp.train();
 })();
