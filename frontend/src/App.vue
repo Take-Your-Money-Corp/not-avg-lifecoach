084fbc4e9ea670403b4cc0eb773f6688d1de52e3
Danielle Macdonald <danielle.macdonald@alumni.ubc.ca>
2021-02-24 13:09:27 -0800

    update main page style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,16 +14,19 @@ export default {
 </script>
 
 <style lang="scss">
-body{
-  background-color: thistle;
+html, body{
+   margin:0;
+   padding:0; 
+   background-color: thistle;
 }
 #app {
+   
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-
-  color: #2c3e50;
+height:100vh;
+  
 }
 
 #nav {
