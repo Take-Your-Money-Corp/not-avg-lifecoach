30d499360f0525694b776543845102a3034b2672
Danielle Macdonald <danielle.macdonald@alumni.ubc.ca>
2021-02-24 11:00:45 -0800

    update colour

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,6 +14,9 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  background-color: thistle;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
