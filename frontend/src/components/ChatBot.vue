6d4d1183c00c5cb92632a41f4e5fe03e031e8b08
Danielle Macdonald <danielle.macdonald@alumni.ubc.ca>
2021-02-24 13:09:38 -0800

    update chatbot styling

--- frontend/src/components/ChatBot.vue
+++ frontend/src/components/ChatBot.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="chat-bot">
-    <p class="bot-message" v-if="showReplyFlag">Bot said: {{ reply }}</p>
-    <p class="user-message" v-if="showMessageFlag">You said: {{ ourMessage }}</p>
-    <input type="text" v-model="ourMessage" />
-    <button @click="sendMessage">Send Message</button>
+    <header>
+      <div class="filler">
+<button class="minimize">
+  <span class="ms-Icon ms-Icon--ChromeMinimize">
+    
+  </span>
+  </button></div>
+      </header>
+    <p class="bot-message" v-if="showReplyFlag">{{ reply }}</p>
+    <p class="user-message" v-if="showMessageFlag">{{ ourMessage }}</p>
+    <div class="user-input">
+   <input class="text-input" type="text" placeholder="Type your message..." v-model="ourMessage" /> 
+<button @click="sendMessage" class="send-message"><img id="send-icon" src="./sendIcon.png" /></button> 
+</div>
   </div>
 </template>
 
@@ -92,11 +102,65 @@ li {
 a {
   color: #42b983;
 }
+header{
+  margin: 0em 0em 5em 0em;
+  background-color:#D65DB1;
+  width: 20em;
+  height:2.5em;
+  display:flex
+}
+.send-message{
+  background-color: white;
+  z-index: 1;
+  border: none;
+  width: 3em;
+  border-radius: 40px;
+}
+.user-input{
+  border-top: grey solid 1px;
+  margin: auto 0em 0em 0em;
+  display: flex;
+}
+.text-input{
+  padding: 1em;
+  border-right: none;
+  border-bottom: none;
+  border-left: none;
+  border-top: none;
+  width: -webkit-fill-available;
+}
+.bot-message{
+  background-color: #FF9671;
+  margin: 0em auto 0em 0.5em;
+  border: grey solid 1px;
+  display: flex;
+  border-radius: 10px;
+  padding: 0.2em;
+  
+}
+.user-message{
+  background-color: #FFC75F;
+  border: grey solid 1px;
+  margin: 0em 0.5em 0em auto;
+  display: flex;
+  border-radius: 10px;
+  padding: 0.2em;
+
+}
+header .filler{
+   flex:0 10000 100%;
+}
+#send-icon{
+  width: 1.75rem;
+}
 
 #chat-bot {
-  padding: 100px;
-  border:#42b983 solid 1px;
-  width: 300px;
+
+  padding: 0px;
+  background-color: white;
+  border:#845EC2 solid 4px;
+  width: 20em;
+  height: 40em;
   display:flex;
   flex-direction: column;
   // justify-self: center;
