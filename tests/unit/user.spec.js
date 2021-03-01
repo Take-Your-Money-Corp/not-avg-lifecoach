9a327bf66f32ae94249eb925426f269fae43874a
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-03-01 02:46:44 -0800

    Make  user component props test (bot was also prop test)

---
+++ tests/unit/user.spec.js
@@ -0,0 +1,23 @@
+import { mount } from "@vue/test-utils";
+import User from "@/components/User.vue";
+
+describe("User.vue", () => {
+  it("Renders the user's message when passed", () => {
+    const userMessage = "I am some text that a user would say";
+    const wrapper = mount(User, {
+      propsData: {
+        conversation: [
+          {
+            chatStyle: "user",
+            message: "I am some text that a user would say"
+          }
+        ],
+        index: 0
+      }
+    });
+
+    expect(
+      wrapper.find('[data-testid="userMessage"]').element.textContent
+    ).toMatch(userMessage);
+  });
+});
