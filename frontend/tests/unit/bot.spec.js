cb8096378539b27dbcec15bcfe81b0eabbd2c59c
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-03-01 02:37:21 -0800

    bot component unit test written

---
+++ frontend/tests/unit/bot.spec.js
@@ -0,0 +1,24 @@
+import { mount } from "@vue/test-utils";
+import Bot from "@/components/Bot.vue";
+
+describe("Bot.vue", () => {
+  it("Renders the bot's message when passed", () => {
+    const botMessage =
+      "Hello, I am your Motivational Lifecoach, ask me anything!";
+    const wrapper = mount(Bot, {
+      propsData: {
+        conversation: [
+          {
+            chatStyle: "bot",
+            message: "Hello, I am your Motivational Lifecoach, ask me anything!"
+          }
+        ],
+        index: 0
+      }
+    });
+
+    expect(
+      wrapper.find('[data-testid="botMessage"]').element.textContent
+    ).toMatch(botMessage);
+  });
+});
