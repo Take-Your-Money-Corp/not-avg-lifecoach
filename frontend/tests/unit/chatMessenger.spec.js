782d3db796e1efe8ced730ac782888ee21a60ca6
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-03-01 16:49:29 -0800

    successfully returns token id after mock axios request

--- frontend/tests/unit/chatMessenger.spec.js
+++ frontend/tests/unit/chatMessenger.spec.js
@@ -1,6 +1,6 @@
 import ChatMessenger from "@/components/ChatMessenger";
 import { mount } from "@vue/test-utils";
-import { getMessage } from "@/services/axios";
+import { makeHandshake } from "@/services/axios";
 import flushPromises from "flush-promises";
 
 jest.mock("@/services/axios");
@@ -8,25 +8,25 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-describe("MessageDisplay", () => {
-  it("Calls getMessage and displays message", async () => {
-    const mockMessage = "Hello from the db";
-    getMessage.mockResolvedValueOnce({ text: mockMessage });
-    const wrapper = mount(MessageDisplay);
+describe("ChatMessenger", () => {
+  it("Makes handshake with backend and returns token id", async () => {
+    const mockToken = "I-R-A-Rest-Token";
+    makeHandshake.mockResolvedValueOnce(mockToken);
+    const wrapper = mount(ChatMessenger);
     await flushPromises();
-    expect(getMessage).toHaveBeenCalledTimes(1);
-    const message = wrapper.find('[data-testid="message"]').element.textContent;
-    expect(message).toEqual(mockMessage);
+    expect(makeHandshake).toHaveBeenCalledTimes(1);
+    const componentToken = wrapper.vm.nlpRestToken;
+    expect(componentToken).toEqual(mockToken);
   });
 
-  it("Displays an error when getMessage call fails", async () => {
-    const mockError = "Oops! Something went wrong.";
-    getMessage.mockRejectedValueOnce({ text: mockError });
-    const wrapper = mount(MessageDisplay);
-    await flushPromises();
-    expect(getMessage).toHaveBeenCalledTimes(1);
-    const error = wrapper.find('[data-testid="message-error"]').element
-      .textContent;
-    expect(error).toEqual(error);
-  });
+  //   it("Displays an error when getMessage call fails", async () => {
+  //     const mockError = "Oops! Something went wrong.";
+  //     getMessage.mockRejectedValueOnce({ text: mockError });
+  //     const wrapper = mount(MessageDisplay);
+  //     await flushPromises();
+  //     expect(getMessage).toHaveBeenCalledTimes(1);
+  //     const error = wrapper.find('[data-testid="message-error"]').element
+  //       .textContent;
+  //     expect(error).toEqual(error);
+  //   });
 });
