3a737ff66191f7845b6a8aa874d4e7bcbeac4f97
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-03-18 01:15:41 -0700

    add components to chat-analysis page and they can show on click

---
+++ frontend/src/components/PartsOfSpeech.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      Parts Of Speech Tagging
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- frontend/src/views/ChatAnalysis.vue
+++ frontend/src/views/ChatAnalysis.vue
@@ -7,6 +7,11 @@
       <button @click="goToChatRoute" class="button-link">Chat</button>
     </b-navbar>
     <h2 class="text-dark">Chat Analysis</h2>
+    <b-container fluid>
+      <SentimentAnalysis v-show="showSentimentAnalysis" />
+      <NamedEntityRecognition v-show="showNamedEntityRecognition" />
+      <PartsOfSpeech v-show="showPartOfSpeech" />
+    </b-container>
     <b-navbar
       toggleable="lg"
       type="dark"
@@ -15,19 +20,57 @@
       class="center"
     >
       <b-navbar-nav class="mx-auto">
-        <b-nav-item href="#">Part of Speech Tagging</b-nav-item>
-        <b-nav-item href="#">Sentiment Analysis</b-nav-item>
-        <b-nav-item href="#">Named Entity Recognition</b-nav-item>
+        <b-nav-item @click="toShowPartOfSpeech"
+          >Part of Speech Tagging</b-nav-item
+        >
+        <b-nav-item @click="toShowSentimentAnalysis"
+          >Sentiment Analysis</b-nav-item
+        >
+        <b-nav-item @click="toShowNamedEntityRecognition"
+          >Named Entity Recognition</b-nav-item
+        >
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
 <script>
+import SentimentAnalysis from "@/components/SentimentAnalysis.vue";
+import NamedEntityRecognition from "../components/NamedEntityRecognition.vue";
+import PartsOfSpeech from "../components/PartsOfSpeech.vue";
+
 export default {
+  data() {
+    return {
+      showSentimentAnalysis: true,
+      showPartOfSpeech: false,
+      showNamedEntityRecognition: false
+    };
+  },
+  components: {
+    SentimentAnalysis,
+    PartsOfSpeech,
+    NamedEntityRecognition
+  },
   methods: {
     goToChatRoute() {
       this.$router.push("/");
+    },
+    toShowSentimentAnalysis() {
+      this.showSentimentAnalysis = true;
+      this.showPartOfSpeech = false;
+      this.showNamedEntityRecognition = false;
+    },
+    toShowPartOfSpeech() {
+      this.showSentimentAnalysis = false;
+      this.showPartOfSpeech = true;
+      this.showNamedEntityRecognition = false;
+    },
+
+    toShowNamedEntityRecognition() {
+      this.showSentimentAnalysis = false;
+      this.showPartOfSpeech = false;
+      this.showNamedEntityRecognition = true;
     }
   }
 };
