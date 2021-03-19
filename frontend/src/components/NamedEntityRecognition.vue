ee855a36c63ca0f66e7729a25db5621c1a6ca500
Matt Borle <43189368+MattB70@users.noreply.github.com>
2021-03-18 23:25:42 -0700

    More skeleton and styling for NER

--- frontend/src/components/NamedEntityRecognition.vue
+++ frontend/src/components/NamedEntityRecognition.vue
@@ -7,13 +7,19 @@
     <div v-if="conversation">
         <b-list-group>
             <div v-for="message in conversation" :key="message.index">
-              <b-list-group-item><h4 align="left">{{ message.chatStyle }} said: </h4><p align="left">{{ message.text }}</p></b-list-group-item>
+
+              <b-list-group-item>
+                <h4 align="left">{{ message.chatStyle }} said: </h4>
+                <p align="left">{{ message.text }}</p>
+                <p align="left" style="font-weight: 600;">Named Entities:</p>
+              </b-list-group-item>
+              
             </div>
         </b-list-group>
     </div>
 
     <div v-else>
-      Please interact with the chatbot to see messages.
+      Please chat with the bot before attempting to inspect messages
     </div>
 
   </div>
@@ -23,14 +29,28 @@
 export default {
   data() {
     return {
-      conversation: undefined
+      conversation: undefined,
+      namedEntities: undefined,
     };
   },
   created() {
     if (this.$store.state.responseScore)
       this.conversation = this.$store.state.conversation;
+
+      for(var i = 0; i < this.conversation.length; i++)
+      {
+        var text = this.conversation[i].text;
+        console.log(text);
+        // perform ner on text
+        // put result for each string at conversation[i] into namedEntities[i]
+      }
   }
 };
 </script>
 
-<style></style>
+<style>
+h4 {
+  color: rgb(116, 0, 170);
+  font-weight: 600;
+}
+</style>
