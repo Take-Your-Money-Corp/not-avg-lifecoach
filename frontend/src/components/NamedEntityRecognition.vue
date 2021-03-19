b96d5af5c995af4fa631d7fd15999cde84e08a5b
Matt Borle <43189368+MattB70@users.noreply.github.com>
2021-03-18 22:33:44 -0700

    messages in list group

--- frontend/src/components/NamedEntityRecognition.vue
+++ frontend/src/components/NamedEntityRecognition.vue
@@ -3,14 +3,19 @@
     <h3>
       Named Entity Recognition
     </h3>
+
     <div v-if="conversation">
-      <div v-for="message in conversation" :key="message.index">
-        {{ message.chatStyle }} said: {{ message.text }}
-      </div>
+        <b-list-group>
+            <div v-for="message in conversation" :key="message.index">
+              <b-list-group-item><h4 align="left">{{ message.chatStyle }} said: </h4><p align="left">{{ message.text }}</p></b-list-group-item>
+            </div>
+        </b-list-group>
     </div>
+
     <div v-else>
-      Please chat
+      Please interact with the chatbot to see messages.
     </div>
+
   </div>
 </template>
 
