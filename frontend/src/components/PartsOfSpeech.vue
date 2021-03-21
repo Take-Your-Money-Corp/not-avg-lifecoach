d34489ca8ced5885d913594dcb44062ee0980a02
Wee Jia Quan <weejiaquan1234@gmail.com>
2021-03-20 19:18:32 -0700

    added try-catch and formatting

--- frontend/src/components/PartsOfSpeech.vue
+++ frontend/src/components/PartsOfSpeech.vue
@@ -31,21 +31,30 @@ export default {
     var f;
     var flag;
     this.flag = 0;
-    for (i = 1; i < this.conversation.length; i = i + 2) {
-      var tagger = posTagger();
-      this.word = tagger.tagSentence(this.conversation[i].text);
-      this.fullText += "<table style=\"width:20%; margin-left: auto; margin-right: auto;\" cellpadding=\"0\" cellspacing=\"0\">"
-      for (f = 0; f < this.word.length; f++) {
-        if (this.flag == 0) {
-          this.fullText += "<tr><h3><b>" + this.conversation[i].text + "</h3></b></tr>";
-          this.fullText += "<tr><th>Word</th><th>Analysis</th></tr>"
+    try {
+      for (i = 1; i < this.conversation.length; i = i + 2) {
+        var tagger = posTagger();
+        this.word = tagger.tagSentence(this.conversation[i].text);
+        this.fullText +=
+          '<table style="width:20%; margin-left: auto; margin-right: auto;" cellpadding="0" cellspacing="0">';
+        for (f = 0; f < this.word.length; f++) {
+          if (this.flag == 0) {
+            this.fullText +=
+              "<tr><h3><b>" + this.conversation[i].text + "</h3></b></tr>";
+            this.fullText += "<tr><th>Word</th><th>Analysis</th></tr>";
+          }
+          this.fullText +=
+            "<tr><td>" +
+            this.word[f].value +
+            "</td><td>" +
+            this.word[f].pos +
+            "</td></tr>";
+          this.flag = 1;
         }
-        this.fullText += "<tr><td>" + this.word[f].value + "</td><td>" + this.word[f].pos + "</td></tr>";
-        this.flag = 1;
+        this.flag = 0;
+        this.fullText += "</table>";
       }
-      this.flag = 0;
-      this.fullText += "</table>";
-    }
+    } catch {}
   }
 };
 </script>
