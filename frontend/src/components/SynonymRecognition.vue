909dca8054dfbeaa514b01c47f4e5e578e3e0c10
Wee Jia Quan <weejiaquan1234@gmail.com>
2021-03-20 23:49:52 -0700

    implement synonymrecognition

--- frontend/src/components/SynonymRecognition.vue
+++ frontend/src/components/SynonymRecognition.vue
@@ -19,7 +19,8 @@ export default {
       conversation: undefined,
       word: undefined,
       sentence: "",
-      fullText: ""
+      fullText: "",
+      thesaurusText: ""
     };
   },
   created() {
@@ -35,21 +36,35 @@ export default {
         var tagger = posTagger();
         this.word = tagger.tagSentence(this.conversation[i].text);
         this.fullText +=
-          '<table style="width:20%; margin-left: auto; margin-right: auto;" cellpadding="0" cellspacing="0">';
+          '<table style="width:70%; margin-left: auto; margin-right: auto; text-align: left;" cellpadding="15" >';
         for (var f = 0; f < this.word.length; f++) {
           if (this.flag == 0) {
             this.fullText +=
               "<tr><h3><b>" + this.conversation[i].text + "</h3></b></tr>";
             this.fullText += "<tr><th>Word</th><th>Analysis</th></tr>";
           }
+          if (thesaurus.find(this.word[f].value).length > 0) {
+            for (var g = 0;g < thesaurus.find(this.word[f].value).length; g++) {
+              if (g == thesaurus.find(this.word[f].value).length - 1) {
+                this.thesaurusText += thesaurus.find(this.word[f].value)[g];
+              } else {
+                this.thesaurusText +=
+                  thesaurus.find(this.word[f].value)[g] + ", ";
+              }
+            }
+          } else {
+            this.thesaurusText = "None";
+          }
+
           this.fullText +=
             "<tr><td>" +
             this.word[f].value +
             "</td><td>" +
-            this.word[f].pos +
+            this.thesaurusText +
             "</td></tr>";
           this.flag = 1;
-            console.log(thesaurus.find(this.word[f].value))
+          this.thesaurusText = "";
+          console.log(thesaurus.find(this.word[f].value));
         }
         this.flag = 0;
         this.fullText += "</table>";
