b073204b78de2bde185eb6b9eb42155aae74fed7
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-03-22 05:40:11 -0700

    Revert "Update SentimentAnalysis.vue"
    This reverts commit d00eddcef8d840c16d5369e7fefa8f4b018bf9ae.

--- frontend/src/components/SentimentAnalysis.vue
+++ frontend/src/components/SentimentAnalysis.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <h3>Sentiment Analysis</h3>
-    <!-- <BotReplyAccordian :response="response" /> -->
-
-    <div v-html="fullText"></div>
+    <BotReplyAccordian :response="response" />
   </div>
 </template>
 
@@ -12,54 +10,17 @@ import BotReplyAccordian from "@/components/BotReplyAccordian.vue";
 
 export default {
   components: {
-    // BotReplyAccordian
+    BotReplyAccordian
   },
   data() {
     return {
-      response: undefined,
-      word: undefined,
-      sentence: "",
-      fullText: ""
+      response: undefined
     };
   },
-  created() {
+  mounted() {
     if (this.$store.state.responseScore) {
       this.response = this.$store.state.responseScore;
-
-      console.log(this.$store);
-      //console.log(this.response.nlp.classifications);
-      //console.log(this.response.nlp);
-      console.log(this.response.nlp.classifications[0].intent);
-      console.log(this.response.nlp.classifications[0].score);
       // console.log(this.response.nlp.classifications);
-      var i;
-      var f;
-      var flag;
-      this.flag = 0;
-      try {
-        for (i = 1; i < this.response.nlp.classifications.length; i++) {
-          this.word = "test";
-          this.fullText +=
-            '<table style="width:20%; margin-left: auto; margin-right: auto;" cellpadding="0" cellspacing="0">';
-          for (f = 0; f < this.word.length; f++) {
-            if (this.flag == 0) {
-              this.fullText +=
-                "<tr><h3><b>" + this.conversation[i].text + "</h3></b></tr>";
-              this.fullText += "<tr><th>Word</th><th>Analysis</th></tr>";
-            }
-            this.fullText +=
-              "<tr><td>" +
-              this.word[f].value +
-              "</td><td>" +
-              this.word[f].pos +
-              "</td></tr>";
-            this.flag = 1;
-          }
-          this.flag = 0;
-          this.fullText += "</table>";
-          console.log("haha");
-        }
-      } catch {}
     }
   }
 };
