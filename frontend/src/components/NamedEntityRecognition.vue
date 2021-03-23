c39f780c6d9de57fb6e7a34704b09fa169a4578e
Matt Borle <43189368+MattB70@users.noreply.github.com>
2021-03-22 17:51:32 -0700

    listing entities

--- frontend/src/components/NamedEntityRecognition.vue
+++ frontend/src/components/NamedEntityRecognition.vue
@@ -12,7 +12,13 @@
                 <h4 align="left">{{ message.chatStyle }} said: </h4>
                 <p align="left">{{ message.text }}</p>
                 <div align="left" style="font-weight: 600;">Named Entities:</div>
-                <div align="left" style="color: rgb(116, 0, 170);">{{message.namedEntities}}</div>
+                <table style="margin: 0.5em;">
+                  <tr>
+                    <th>Type</th>
+                    <th>Value</th>
+                  </tr>
+                  <tr style="color: rgb(116, 0, 170);" v-for="entity in message.namedEntities" :key="entity.index"><td align="left" style="padding: 0.1em;">{{entity.tag}}</td><td align="left" style="padding: 0.1em;">"{{entity.value}}"</td></tr>
+                </table>
               </b-list-group-item>
               
             </div>
@@ -40,13 +46,43 @@ export default {
   },
   methods: {
     NER() {
-      for(var i = 0; i < this.conversation.length; i++)
-      {
-        this.conversation[i].namedEntities = i;
-        // perform ner on text
-        // put result for each string at conversation[i].namedEntities
-        // Format: [type0, name0, type0, name0, ... , typen, namen]
-      }
+      // Load wink ner.
+      var ner = require( 'wink-ner' );
+      // Create your instance of wink ner & use default config.
+      var myNER = ner();
+      // Define training data.
+      var trainingData = [
+        { text: 'manchester united', entityType: 'club', uid: 'manu' },
+        { text: 'manchester', entityType: 'city' },
+        { text: 'U K', entityType: 'country', uid: 'uk' },
+        { text: 'danielle', entityType: 'name' },
+        { text: 'matt', entityType: 'name' },
+        { text: 'canada', entityType: 'country' },
+        { text: 'united states', entityType: 'country' },
+        { text: 'U S', entityType: 'country', uid: 'us' },
+        { text: 'spain', entityType: 'country' },
+        { text: 'kelowna', entityType: 'city' },
+      ];
+      // Learn from the training data.
+      myNER.learn( trainingData );
+      // Since recognize() requires tokens, use wink-tokenizer.
+      var winkTokenizer = require( 'wink-tokenizer' );
+      // Instantiate it and extract tokenize() api.
+      var tokenize = winkTokenizer().tokenize;
+
+      if(this.conversation != null){
+        for(var i = 0; i < this.conversation.length; i++)
+        {
+          this.conversation[i].namedEntities = i;
+          // Tokenize the sentence
+          var tokens = tokenize(this.conversation[i].text);
+          // Detect entities
+          tokens = myNER.recognize(tokens);
+
+          // put result for each string at conversation[i].namedEntities
+          this.conversation[i].namedEntities = tokens;
+        }
+      }else{console.log("Conversation is empty or undefined!")}
     }
   }
 };
@@ -57,4 +93,6 @@ h4 {
   color: rgb(116, 0, 170);
   font-weight: 600;
 }
+tr:nth-child(even) {background-color: #f7f7f7;}
+tr:hover {background-color: rgb(234, 225, 238);}
 </style>
